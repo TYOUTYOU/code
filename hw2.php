94f87dbd0c0a5e9cf390ff5242a43ce164d35bc5
tyoutyou <tyoutyou1991@gmail.com>
2017-01-24 23:00:17 +0900

    ID,ユーザー名と投稿内容を入れているかどうかをチェックする

--- hw2.php
+++ hw2.php
@@ -16,51 +16,72 @@
             <input type="submit" name="sub" value="投稿">
         </form>
 
-        <?php  //入力した内容をデータベースに登録する
+        <?php
         require_once 'hw2_dbm.php';
         $id = isset($_POST['id'])? htmlspecialchars($_POST['id']) : null;;
         $name = isset($_POST['name'])? htmlspecialchars($_POST['name']) : null;;
         $con = isset($_POST['con'])? htmlspecialchars($_POST['con']) : null;;
 
-        if($id!='' and $name!='' and $con!='' and !empty($_POST['sub'])) { //id,ユーザー名、本文に内容を入れているかどうかを確認する
-            try {
-                $db = getDb();
-                $s = $db->prepare('INSERT INTO usercontents(id,name,contents)VALUES(:id, :name, :con)');//データベースにデータを入れる
-                $s->bindValue(':id', $_POST['id']);
-                $s->bindValue(':name', $_POST['name']);
-                $s->bindValue(':con', $_POST['con']);
-                $s->execute();
-                $db = NULL;
-                header('Location: http://localhost/hw2.php'); //リダイレクト
-                exit;
-            } catch (PDOException $e) {
-                die("エラーメッセージ:{$e->getMessage()}");
+
+        //ID,ユーザー名と投稿内容を入力したかどうかをチェックする
+        if(!empty($_POST['sub'])) {
+            if($id === "" && $name === "" && $con === "" && !empty($_POST['sub'])){
+                print "ID、ユーザー名、投稿内容を入れてください。";
+            }elseif ($id === "" && $name !== "" && $con !== "" && !empty($_POST['sub'])) {
+                print  "IDを入れてください。";
+            }elseif ($id === "" && $name == "" && $con !== "" && !empty($_POST['sub'])) {
+                print  "IDとユーザー名を入れてください。";
+            }elseif($id !== "" && $name == "" && $con !== "" && !empty($_POST['sub'])){
+                print  "ユーザー名を入れてください。";
+            }elseif ($id == "" && $name !== "" && $con == "" && !empty($_POST['sub'])){
+                print "IDと投稿内容を入れてください。";
+            }elseif($id !== "" && $name == "" && $con == "" && !empty($_POST['sub'])){
+                print "ユーザー名と投稿内容を入れてください。";
+            }elseif($id !== "" && $name !== "" && $con == "" && !empty($_POST['sub'])){
+                print "投稿内容を入れてください。";
+            }
+
+            //ID,ユーザー名と投稿内容を全部入力したら、データベースに接続し、入力したデータをデータベースに入れる
+            if ($id !== "" && $name !== "" && $con !== "" && !empty($_POST['sub'])) {
+                try {
+                    $db = getDb();
+                    $s = $db->prepare('INSERT INTO post(id,name,contents)VALUES(:id, :name, :con)');//データベースにデータを入れる
+                    $s->bindValue(':id', $_POST['id']);
+                    $s->bindValue(':name', $_POST['name']);
+                    $s->bindValue(':con', $_POST['con']);
+                    $s->execute();
+                    $db = NULL;
+                    header('Location: http://localhost/hw2.php'); //リダイレクト
+                    exit;
+                } catch (PDOException $e) {
+                    die("エラーメッセージ:{$e->getMessage()}");
+                }
             }
         }
-         ?>
+        ?>
     </section>
 
     <section>
         <h2>投稿内容</h2>
-        <?php //データベースのなかのデータを表示する
+        <?php
         require_once 'Encode.php';
-       try {
-           $db = getDb();
-           $s = $db->prepare('SELECT * FROM usercontents ORDER BY id DESC ');
-           $s->execute();
-           while ($row = $s->fetch(PDO::FETCH_ASSOC)) {
-               ?>
-               <tr>
-                   <td><?php echo 'ID:'; e($row['id']);?></td>
-                   <td><?php echo 'ユーザー名:'; e($row['name']);?></td></br>
-                   <td><?php e($row['contents']);?></td>
-               </tr>
-               <hr>
-               <?php
-           }
-           $db = NULL;
-       }catch (PDOException $e) {
-               die("エラーメッセージ:{$e->getMessage()}");
+        try {
+            $db = getDb();
+            $s = $db->prepare('SELECT * FROM post ORDER BY id DESC ');
+            $s->execute();
+            while ($row = $s->fetch(PDO::FETCH_ASSOC)) {
+                ?>
+                <tr>
+                    <td><?php echo 'ID:'; e($row['id']);?></td>
+                    <td><?php echo 'ユーザー名:'; e($row['name']);?></td></br>
+                    <td><?php e($row['contents']);?></td>
+                </tr>
+                <hr>
+                <?php
+            }
+            $db = NULL;
+        }catch (PDOException $e) {
+            die("エラーメッセージ:{$e->getMessage()}");
         }
         ?>
     </section>
